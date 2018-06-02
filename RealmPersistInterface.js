19ea35d02c6099a9cdbd767d57f9520d767e30df
Unknown <palumbo.alb@gmail.com>
2018-06-02 15:08:14 -0400

    Change callbacks to promises

--- RealmPersistInterface.js
+++ RealmPersistInterface.js
@@ -16,73 +16,82 @@ class RealmPersistInterface {
         this.items = this.realm.objects('Item');
     }
 
-    getItem = (key, callback) => {
-        try {
-            const matches = this.items.filtered(`name = "${key}"`);
+    getItem = (key) => {
+        return new Promise((resolve, reject) => {
+          try {
+              const matches = this.items.filtered(`name = "${key}"`);
 
-            if (matches.length > 0 && matches[0]) {
-                callback(null, matches[0].content);
-            } else {
-                throw new Error(`Could not get item with key: '${key}'`);
-            }
-        } catch (error) {
-            callback(error);
-        }
+              if (matches.length > 0 && matches[0]) {
+                  resolve(null, matches[0].content);
+              } else {
+                  reject(new Erro(`Could not get item with key: '${key}'`));
+              }
+          } catch (error) {
+              reject(error);
+          }
+        })
     };
 
-    setItem = (key, value, callback) => {
-        try {
-            this.getItem(key, (error) => {
-                this.realm.write(() => {
-                    if (error) {
-                        this.realm.create(
-                            'Item',
-                            {
-                                name: key,
-                                content: value,
-                            }
-                        );
-                    } else {
-                        this.realm.create(
-                            'Item',
-                            {
-                                name: key,
-                                content: value,
-                            },
-                            true
-                        );
-                    }
+    setItem = (key, value) => {
+        return new Promise((resolve, reject) => {
+          try {
+              this.getItem(key, (error) => {
+                  this.realm.write(() => {
+                      if (error) {
+                          this.realm.create(
+                              'Item',
+                              {
+                                  name: key,
+                                  content: value,
+                              }
+                          );
+                      } else {
+                          this.realm.create(
+                              'Item',
+                              {
+                                  name: key,
+                                  content: value,
+                              },
+                              true
+                          );
+                      }
 
-                    callback();
-                });
-            });
-        } catch (error) {
-            callback(error);
-        }
+                      resolve();
+                  });
+              });
+          } catch (error) {
+              reject(error);
+          }
+        })
     };
 
-    removeItem = (key, callback) => {
-        try {
-            this.realm.write(() => {
-                const item = this.items.filtered(`name = "${key}"`);
+    removeItem = (key) => {
+        return new Promise((resolve, reject) => {
+          try {
+              this.realm.write(() => {
+                  const item = this.items.filtered(`name = "${key}"`);
 
-                this.realm.delete(item);
-            });
-        } catch (error) {
-            callback(error);
-        }
+                  this.realm.delete(item);
+              });
+              resolve();
+          } catch (error) {
+              reject(error);
+          }
+        })
     };
 
-    getAllKeys = (callback) => {
-        try {
-            const keys = this.items.map(
-                (item) => item.name
-            );
+    getAllKeys = () => {
+        return new Promise((resolve, reject) => {
+          try {
+              const keys = this.items.map(
+                  (item) => item.name
+              );
 
-            callback(null, keys);
-        } catch (error) {
-            callback(error);
-        }
+              resolve(null, keys);
+          } catch (error) {
+              reject(error);
+          }
+        })
     };
 }
 
